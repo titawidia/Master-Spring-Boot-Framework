ea129e1c381af1a7b82e6d2318c12648368df3c0
titawidia <gitawidia166@gmail.com>
2023-01-11 09:57:46 +0700

    Initial commit

---
+++ src/main/java/com/itc/masterspringbootframework/MasterSpringBootFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.itc.masterspringbootframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSpringBootFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterSpringBootFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itc/masterspringbootframework/MasterSpringBootFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.masterspringbootframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSpringBootFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
